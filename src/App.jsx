3220074de80a82b839d37b7db68a7e18329ca700
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-10 12:08:22 +0530

    Upload 01

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/Counter';
+import ChatBot from './components/ChatBot';
+
+function App() {
+
+  return (
+    <>
+      {/* <Counter /> */}
+      <ChatBot />
+    </>
+  )
+}
+
+export default App;
