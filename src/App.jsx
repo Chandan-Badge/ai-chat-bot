f55e7177191f5ebcf52eaf6f8b9d5f507be1f901
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-10 15:37:19 +0530

    Upload 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Counter from './components/Counter';
 import ChatBot from './components/ChatBot';
 
 function App() {
 
   return (
     <>
-      {/* <Counter /> */}
       <ChatBot />
     </>
   )
