f3a938f8496625567d5138c59bc868ee15adbf10
Faraz Mirza <farazmirza1023@gmail.com>
2025-03-10 23:57:28 +0530

    Response fixed

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -2,9 +2,17 @@ import ChatBotIcon from "./ChatBotIcon";
 
 function ChatMessage({ chat }) {
     return (
-        <div className={`message ${chat.role === "model" ? "bot" : "user"}-message flex gap-3 flex-col ${chat.role === "model" ? "items-start" : "items-end"}`}>
-            {chat.role === "model" && <ChatBotIcon />}
-            <p className="message-text px-4 py-3 max-w-[75%] text-[15px] bg-[#6d4fc2] text-[#fff] rounded-s-xl rounded-tr-xl rounded-br-sm">
+        <div className={`message flex gap-3 ${chat.role === "model" ? "items-start" : "items-end"} flex-col`}>
+            {chat.role === "model" && (  
+                <div className="flex items-center justify-center w-8 h-8">
+                    <ChatBotIcon /> 
+                </div>
+            )}
+            <p className={`message-text px-4 py-3 max-w-[75%] text-[15px] 
+                ${chat.role === "model" 
+                    ? "bg-[#f1f1f1] text-black rounded-s-xl rounded-tr-xl rounded-br-sm"  
+                    : "bg-[#6d4fc2] text-white rounded-e-xl rounded-tl-xl rounded-bl-sm"
+                }`}>
                 {chat.text}
             </p>
         </div>
