2e78373604076e6d8c56b130d0032004e0272e0f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-03-13 20:13:52 +0530

    Improvement 09

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -16,9 +16,9 @@ function ChatBot() {
 
     const generateBotResponse = async (history) => {
 
-        const updateHistory = (text) => {
+        const updateHistory = (text, isError = false) => {
             // Helper function to update chat history with bot's response
-            setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."), { role: "model", text }]);
+            setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."), { role: "model", text, isError }]);
         };
 
         // Remove the "Thinking..." placeholder
@@ -45,7 +45,7 @@ function ChatBot() {
             const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
             updateHistory(apiResponseText);
         } catch (error) {
-            console.error("Error:", error);
+            updateHistory(error.massage, true);
         }
     };
 
@@ -66,16 +66,16 @@ function ChatBot() {
     return (
         <div className={`container font-[poppins] ${showChatBot ? "show-chatbot" : ""}`}>
 
-        <button onClick={changeBtn} className="chatbot-toggler fixed bottom-8 right-9 border-none h-12 w-12 flex cursor-pointer rounded-full bg-[#6d4fc2] justify-center items-center">
+        <button onClick={changeBtn} className="chatbot-toggler fixed bottom-5 sm:bottom-8 right-5 sm:right-9 border-none h-12 w-12 flex cursor-pointer rounded-full bg-[#6d4fc2] justify-center items-center">
             {/* <span className={`material-symbols-outlined absolute text-[#fff] ${condition1} transition-all duration-200 ease-out`}>mode_comment</span> */}
-            <i className={`fa-solid fa-message absolute text-xl text-[#fff] ${condition1} transition-all duration-200 ease-in`}></i>
+            <i className={`fa-solid fa-message absolute text-xl text-[#fff] ${condition1}`}></i>
             <span className={`material-symbols-outlined absolute text-[#fff] ${condition2} transition-all duration-200 ease-out`}>close</span>
             {/* <i className={`fa-solid fa-xmark absolute text-xl text-[#fff] ${condition2} transition-all duration-200 ease-in`}></i> */}
         </button>
 
-            <div className="chatbot-popup fixed opacity-0 pointer-events-none bottom-24 right-9 w-[420px] overflow-hidden bg-[#fff] rounded-lg shadow-lg transition-all scale-50 duration-100 ease-in">
+            <div className="chatbot-popup fixed opacity-0 pointer-events-none bottom-0 sm:bottom-24 right-0 sm:right-9 w-full sm:w-[420px] h-screen sm:h-max overflow-hidden bg-[#fff] rounded-none sm:rounded-lg shadow-lg transition-all scale-50 duration-100 ease-in">
                 {/* ChatBox header  */}
-                <div className="chat-header flex px-6 py-4 items-center justify-between bg-[#6d4fc2]">
+                <div className="chat-header flex px-4 sm:px-6 py-3 sm:py-4 items-center justify-between bg-[#6d4fc2]">
                     <div className="header-info flex gap-3 items-center">
 
                         <ChatBotIcon />
@@ -86,7 +86,7 @@ function ChatBot() {
                 </div>
 
                 {/* Chat body */}
-                <div ref={chatBotRef} className="chat-body h-[460px] overflow-y-auto p-6 mb-16 flex flex-col gap-5">
+                <div ref={chatBotRef} className="chat-body h-[460px] overflow-y-auto py-6 px-4 sm:p-6 mb-16 flex flex-col gap-5">
                     <div className="massage bot-massage flex gap-3 items-center">
                         {/* <ChatBotIcon className="fill-[#fff] bg-[#6d4fc2]" /> */}
                         <svg className="h-9 w-9 p-2 fill-[#fff] bg-[#6d4fc2] rounded-full" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024"><path d="M738.3 287.6H285.7c-59 0-106.8 47.8-106.8 106.8v303.1c0 59 47.8 106.8 106.8 106.8h81.5v111.1c0 .7.8 1.1 1.4.7l166.9-110.6 41.8-.8h117.4l43.6-.4c59 0 106.8-47.8 106.8-106.8V394.5c0-59-47.8-106.9-106.8-106.9zM351.7 448.2c0-29.5 23.9-53.5 53.5-53.5s53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5-53.5-23.9-53.5-53.5zm157.9 267.1c-67.8 0-123.8-47.5-132.3-109h264.6c-8.6 61.5-64.5 109-132.3 109zm110-213.7c-29.5 0-53.5-23.9-53.5-53.5s23.9-53.5 53.5-53.5 53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5zM867.2 644.5V453.1h26.5c19.4 0 35.1 15.7 35.1 35.1v121.1c0 19.4-15.7 35.1-35.1 35.1h-26.5zM95.2 609.4V488.2c0-19.4 15.7-35.1 35.1-35.1h26.5v191.3h-26.5c-19.4 0-35.1-15.7-35.1-35.1zM561.5 149.6c0 23.4-15.6 43.3-36.9 49.7v44.9h-30v-44.9c-21.4-6.5-36.9-26.3-36.9-49.7 0-28.6 23.3-51.9 51.9-51.9s51.9 23.3 51.9 51.9z" /></svg>
